629cfafc6d07a617920c23c9f7be69c1377948af
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2024-01-21 18:36:55 +0100

    dataset better mask

--- src/dataset.py
+++ src/dataset.py
@@ -32,13 +32,23 @@ class ClothesDataset(data.Dataset):
     def __getitem__(self, index):
         img_name = self.img_names[index]
 
-        img = Image.open(path.join(self.data_path, 'image', img_name)).convert('RGB')
-        img = transforms.Resize(self.load_width)(img)
+        img_pil = Image.open(path.join(self.data_path, 'image', img_name)).convert('RGB')
+        img = transforms.Resize(self.load_width)(img_pil)
         img = self.transform(img)
 
         agnostic_mask = Image.open(path.join(self.data_path, 'agnostic-mask', img_name.replace(".jpg","_mask.png"))).convert('RGB')
         agnostic_mask = transforms.Resize(self.load_width)(agnostic_mask)
         agnostic_mask = self.transform(agnostic_mask)
+
+        mask_body = Image.open(path.join(self.data_path, 'image-parse-v3', img_name.replace(".jpg",".png"))).convert('RGBA')
+        mask, mask_body = self.get_body_color_mask(mask_body)
+        mask_body = transforms.Resize(self.load_width)(mask_body)
+        mask_body = self.transform(mask_body)
+
+        img_masked = Image.composite(img_pil, Image.new('RGBA', img_pil.size), mask)
+        img_masked_rgb = img_masked.convert('RGB')
+        img_masked_rgb = transforms.Resize(self.load_width)(img_masked_rgb)
+        img_masked_rgb = self.transform(img_masked_rgb)
 
         cloth = Image.open(path.join(self.data_path, 'cloth', img_name)).convert('RGB')
         cloth = transforms.Resize(self.load_width)(cloth)
@@ -51,11 +61,35 @@ class ClothesDataset(data.Dataset):
         result = {
             'img_name': img_name,
             'img': img,
+            'img_masked': img_masked_rgb,
             'agnostic_mask': agnostic_mask,
+            'mask_body': mask_body,
             'cloth': cloth,
             'cloth_mask': cloth_mask,
         }
         return result
+    
+    @staticmethod
+    def get_body_color_mask(mask_body):
+        target_color = (254, 85, 0)  # #fe5500 in RGB
+
+        opaque = (255, 255, 255, 255)  # White, fully opaque
+        transparent = (255, 255, 255, 0)  # White, fully transparent
+
+        data = mask_body.getdata()
+        new_data = []
+        for item in data:
+            new_data.append(opaque if item[:3] == target_color else transparent)
+
+        mask = Image.new('RGBA', mask_body.size)
+        mask.putdata(new_data)
+
+        result = Image.composite(mask_body, Image.new('RGBA', mask_body.size, transparent), mask)
+
+        result_rgb = result.convert('RGB')
+
+        return mask, result_rgb
+
 
 class ClothesDataLoader:
     def __init__(self, opt, dataset):
