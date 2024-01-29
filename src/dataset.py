887174c294a62241a2be39673a789389ba7f4476
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2024-01-29 10:40:04 +0100

    Fully working most transforms on torch

--- src/dataset.py
+++ src/dataset.py
@@ -61,7 +61,7 @@ class MakeSquareWithPad:
         self.fill = fill  # fill color, 0 for black
 
     def __call__(self, img):
-        _, width, height = img.size()
+        _, height, width = img.size()
         max_side = max(width, height)
         padding_left = padding_right = padding_top = padding_bottom = 0
 
@@ -87,7 +87,136 @@ class ToFloatTensor(object):
     def __repr__(self):
         return self.__class__.__name__ + '()'
 
+class RGBAtoRGBWhiteBlack:
 
+    def __call__(self, img):
+        if img.shape[0] == 4:
+            rgb_img, alpha_img = img[:3, :, :], img[3:4, :, :]
+
+            white = torch.ones_like(rgb_img)
+            black = torch.zeros_like(rgb_img)
+
+            rgb_transformed = torch.where(alpha_img > 0, white, black)
+
+            return rgb_transformed
+        else:
+            return img
+
+class SingleChannelToRGBTransform:
+    def __call__(self, img):
+        if img.shape[0] != 1:
+            raise ValueError("Image must have a single channel")
+
+        # Replicate the single channel across RGB dimensions
+        return img.repeat(3, 1, 1)
+
+
+
+class CropAlphaChannelTransform:
+    def __call__(self, img):
+        # Assuming the alpha channel is the last channel
+        alpha_channel = img[-1]
+
+        # Find rows and columns where all values are not zero
+        non_zero_rows = torch.any(alpha_channel != 0, dim=1)
+        non_zero_cols = torch.any(alpha_channel != 0, dim=0)
+
+        # Crop the image
+        cropped_img = img[:, non_zero_rows, :]
+        cropped_img = cropped_img[:, :, non_zero_cols]
+
+        return cropped_img
+
+
+class AlphaToBlackTransform:
+    def __call__(self, img):
+        if img.shape[0] != 4:
+            raise ValueError("Image must have 4 channels")
+
+        # Separate alpha channel and RGB channels
+        alpha = img[3].unsqueeze(0)  # Make alpha channel have shape (1, H, W)
+        rgb = img[:3]  # First three channels
+
+        # Create a black background
+        background = torch.zeros_like(rgb)
+
+        # Blend the RGB image and background using the alpha channel
+        return torch.where(alpha > 0, rgb, background)
+
+
+class PadToShapeTransform:
+    def __init__(self, output_shape):
+        self.output_shape = output_shape  # Expected shape (C, H, W)
+
+    def __call__(self, img):
+        _, h, w = img.shape
+        target_h, target_w = self.output_shape[1], self.output_shape[2]
+
+        # Calculate padding
+        pad_vert = max((target_h - h) // 2, 0)
+        pad_horiz = max((target_w - w) // 2, 0)
+        
+        # Pad the image
+        padded_img = F.pad(img, (pad_horiz, pad_horiz, pad_vert, pad_vert))
+
+        # Handle case where image is larger than target in any dimension
+        if padded_img.shape[1] > target_h or padded_img.shape[2] > target_w:
+            padded_img = padded_img[:, :target_h, :target_w]
+
+        return padded_img
+
+class ApplyMaskTransform:
+    def __call__(self, img, mask):
+        if img.shape[1:] != mask.shape[1:]:
+            raise ValueError("RGB image and mask must have the same height and width")
+
+        # Ensure mask is 3D (1, H, W)
+        if mask.dim() == 2:
+            mask = mask.unsqueeze(0)
+
+        # Use torch.where to apply mask
+        black_background = torch.zeros_like(img)
+        return torch.where(mask > 0, img, black_background)
+
+
+
+def plot_alpha_channel(img):
+    from matplotlib import pyplot as plt
+    if img.shape[0] < 4:
+        raise ValueError("Image does not have an alpha channel")
+
+    alpha_channel = img[-1]  # Last channel as alpha
+
+    plt.imshow(alpha_channel, cmap='gray')
+    plt.title("Alpha Channel")
+    plt.colorbar()
+    plt.show()
+
+def plot_alpha_channel_raw(img):
+
+    if len(img.shape) != 3:
+        raise ValueError("Image is not 3D")
+    
+    if img.shape[0] != 1:
+        raise ValueError("Image does not have an alpha channel")
+    
+    img = img[0]
+    from matplotlib import pyplot as plt
+    plt.imshow(img, cmap='gray')
+    plt.title("Alpha Channel")
+    plt.colorbar()
+    plt.show()
+
+def plot_rgb(img):
+    from matplotlib import pyplot as plt
+    if img.shape[0] < 3:
+        raise ValueError("Image does not have an RGB channel")
+
+    rgb = img[:3]  # First 3 channels as RGB
+
+    plt.imshow(rgb.permute(1, 2, 0))
+    plt.title("RGB")
+    plt.show()
 
 class ClothesDataset(data.Dataset):
 
@@ -120,9 +249,9 @@ class ClothesDataset(data.Dataset):
         self.rotation_angle = rotation_angle
         self.data_path = path.join(dataset_dir, dataset_mode)
         self.transform = transforms.Compose([
+            RGBAtoRGBWhiteBlack(),
             MakeSquareWithPad(),
             ToFloatTensor(),
-            transforms.Resize(self.load_width),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
         dataset_list = f'{dataset_mode}_pairs.txt'
@@ -163,67 +292,76 @@ class ClothesDataset(data.Dataset):
 
         img = self.transform(img_pil)
 
-        agnostic_mask = io.read_image(os.path.join(self.data_path, 'agnostic-mask', img_name.replace(".jpg","_mask.png")))
-        agnostic_mask = agnostic_mask[:3, :, :]
-        if horizontal_flip:
-            agnostic_mask = F.hflip(agnostic_mask)
-        if angle:
-            agnostic_mask = random_rotation(agnostic_mask)
-        agnostic_mask = self.transform(agnostic_mask)
+        # agnostic_mask = io.read_image(os.path.join(self.data_path, 'agnostic-mask', img_name.replace(".jpg","_mask.png")))
+        # agnostic_mask = agnostic_mask[:3, :, :]
+        # if horizontal_flip:
+        #     agnostic_mask = F.hflip(agnostic_mask)
+        # if angle:
+        #     agnostic_mask = random_rotation(agnostic_mask)
+        # agnostic_mask = self.transform(agnostic_mask)
 
         mask_body_parts = self.convert_to_rgb_tensor(path.join(self.data_path, 'image-parse-v3', img_name.replace(".jpg",".png")))
-        print(mask_body_parts.shape)
+        # print(mask_body_parts.shape)
         if horizontal_flip:
             mask_body_parts = F.hflip(mask_body_parts)
         if zoom:
             mask_body_parts = random_zoom(mask_body_parts)
         if angle:
             mask_body_parts = random_rotation(mask_body_parts)
-
+        
+        # print(f'Mask body parts shape: {mask_body_parts.shape}')
+        # print(f'Img shape: {img.shape}')
         target_colors = [(254, 85, 0), (0, 0, 85), (0,119,220), (85,51,0)]
-        mask, mask_body = self.get_body_color_mask(mask_body_parts, target_colors)
-        # centered_mask_body, offset = self.center_masked_area(mask_body, mask)
-        # mask_body = self.transform(mask_body)
+        mask_tensor = self.get_body_color_mask(mask_body_parts, target_colors, img_pil)
+        # centered_mask_body_pil, offset = self.center_masked_area(mask_pil)
+        # from matplotlib import pyplot as plt
+        # plt.imshow(mask_pil)
+        # t = transforms.ToTensor()
+        # mask = t(mask_tensor)
+        mask_tensor = self.transform(mask_tensor)
+        # centered_mask_body = t(centered_mask_body_pil)
         # centered_mask_body = self.transform(centered_mask_body)
-        mask_body_parts = mask_body_parts[:3, :, :]
+        # mask_body_parts = mask_body_parts[:3, :, :]
         # mask_body_parts = self.transform(mask_body_parts)
 
         # img_masked_rgb = self.adjust_at_offset(img_pil, offset, mask=mask)
         # img_masked_rgb = self.transform(img_masked_rgb)
 
-        # cloth = io.read_image(path.join(self.data_path, 'cloth', img_name))
-        # if horizontal_flip:
-        #     cloth = F.hflip(cloth)
-        # if jitter:
-        #     cloth = color_jitter(cloth)
+        cloth = io.read_image(path.join(self.data_path, 'cloth', img_name))
+        if horizontal_flip:
+            cloth = F.hflip(cloth)
+        if jitter:
+            cloth = color_jitter(cloth)
 
-        # cloth_mask = io.read_image(path.join(self.data_path, 'cloth-mask', img_name))
-        # cloth_color_mask, _ = self.get_body_color_mask(cloth_mask, [(255, 255, 255)])
+        cloth_mask = io.read_image(path.join(self.data_path, 'cloth-mask', img_name))
+        # print(f'Cloth mask shape: {cloth_mask.shape}')
+        # plot_alpha_channel_raw(cloth_mask)
+        target = ApplyMaskTransform()(cloth, cloth_mask)
         # predict = self.adjust_at_offset(cloth, None, mask=cloth_color_mask)
 
         # cloth = self.transform(cloth)
-        # cloth_mask = cloth_mask[:3, :, :]
-        # cloth_mask = self.transform(cloth_mask)
-        # predict = self.transform(predict)
+        cloth_mask = SingleChannelToRGBTransform()(cloth_mask)
+        cloth_mask = self.transform(cloth_mask)
+        target = self.transform(target)
 
 
         result = {
             'img_name': img_name,
-            'img': img,
+            # 'img': img,
             # 'img_masked': img_masked_rgb,
-            'agnostic_mask': agnostic_mask,
-            'mask_body': mask_body,
-            'mask_body_parts': mask_body_parts,
-            # 'centered_mask_body': centered_mask_body,
+            # 'agnostic_mask': agnostic_mask,
+            # 'mask_body': mask_tensor,
+            # 'mask_body_parts': mask_body_parts,
+            'centered_mask_body': mask_tensor,
             # 'cloth': cloth,
-            # 'cloth_mask': cloth_mask,
-            # 'predict': predict
+            'cloth_mask': cloth_mask,
+            'target': target
         }
         return result
     
         
     @staticmethod
-    def get_body_color_mask(mask_body, target_colors):
+    def get_body_color_mask(mask_body, target_colors, img):
         device = "cpu"
         t = transforms.ToPILImage()
         mask_body = t(mask_body.cpu())
@@ -232,57 +370,40 @@ class ClothesDataset(data.Dataset):
 
         mask = torch.any(torch.all(data[:, :, None, :3] == target_colors, dim=-1), dim=-1)
 
-        new_data = torch.zeros(mask_body.size[1], mask_body.size[0], 4, dtype=torch.uint8, device=device)
-        new_data[:, :, :3] = data[:, :, :3]
-        new_data[:, :, 3][mask] = 255
+        new_data = torch.zeros(4, img.size(1), img.size(2), dtype=torch.uint8, device=device)
+        new_data[:3, :, :] = img[:3, :, :]
+        new_data[3, :, :][mask] = 255
 
-        mask_image = Image.fromarray(new_data.cpu().numpy(), 'RGBA')
-        result = Image.new('RGB', mask_body.size)
-        result.paste(mask_body, mask=mask_image)
-
-        t = transforms.ToTensor()
-        mask_image = t(mask_image)
-        result = t(result)
-
-        return mask_image, result
+        
+        new_data = CropAlphaChannelTransform()(new_data)
+        new_data = AlphaToBlackTransform()(new_data)
+        new_data = PadToShapeTransform(img.shape)(new_data)
+        
+        return new_data
     
     @staticmethod
     def adjust_at_offset(img, offset, mask=None):
-        _, height, width = img.shape
-        print(img.shape)
-        new_img = torch.zeros_like(img)
-
-        # Applying offset
-        dx, dy = offset
-        new_img[:, dy:height, dx:width] = img[:, :height-dy, :width-dx]
-
-        # Applying mask if provided
-        if mask is not None:
-            alpha_mask = mask[-1,:,:].unsqueeze(0).expand_as(new_img)
-            alpha_mask = alpha_mask.to(dtype=new_img.dtype)
-            new_img *= alpha_mask
-
+        new_img = Image.new('RGB', img.size)
+        new_img.paste(img, offset, mask=mask)
         return new_img
 
-
     @staticmethod
-    def center_masked_area(img, mask):
-        mask_alpha = mask[0, :, :].cpu().numpy()
+    def center_masked_area(mask):
+        mask_alpha = np.array(mask)[:, :, 3]
 
         rows = np.any(mask_alpha, axis=1)
         cols = np.any(mask_alpha, axis=0)
-        print(rows.shape, cols.shape)
-        print(rows, cols)
         ymin, ymax = np.where(rows)[0][[0, -1]]
         xmin, xmax = np.where(cols)[0][[0, -1]]
 
         masked_center = ((xmin + xmax) // 2, (ymin + ymax) // 2)
 
-        img_center = (img.size(2) // 2, img.size(1) // 2)
+        img_center = (mask.width // 2, mask.height // 2)
 
         offset = (img_center[0] - masked_center[0], img_center[1] - masked_center[1])
 
-        new_img = ClothesDataset.adjust_at_offset(img, offset, mask=mask)
+        new_img = Image.new('RGB', mask.size)
+        new_img.paste(mask, offset, mask=mask)
 
         return new_img, offset
 
