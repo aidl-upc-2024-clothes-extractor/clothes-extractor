661dfcfc9f6ef2ec675d1c0214155a401ae4b541
Joan Puig <joanpuigsanz@gmail.com>
2024-02-12 14:15:59 +0100

    Update model

--- entrypoint.py
+++ entrypoint.py
@@ -107,10 +107,11 @@ def main():
     print("Done")
 
     model = smp.Unet(
-        encoder_name="efficientnet-b0",  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+        encoder_name="resnet34",  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
         encoder_weights="imagenet",  # use `imagenet` pre-trained weights for encoder initialization
         in_channels=3,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
         classes=3,  # model output channels (number of classes in your dataset)
+        decoder_attention_type="scse"
     ).to(device)
 
     optimizer = optim.Adam(model.parameters(), lr=cfg.learning_rate)
