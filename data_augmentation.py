a61772c11da376122990672728b4c48cae8cbaa9
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2024-02-16 22:50:03 +0100

    undo random noise background

--- data_augmentation.py
+++ data_augmentation.py
@@ -139,11 +139,10 @@ class AlphaToBlackTransform:
         alpha = img[3].unsqueeze(0)
         rgb = img[:3]
 
-        rgb_float = rgb.to(torch.float32) / 255.0
-
-        background = torch.randn_like(rgb_float) * torch.std(rgb_float) + torch.mean(rgb_float)
-
-        background = (background * 255).to(torch.uint8)
+        #rgb_float = rgb.to(torch.float32) / 255.0
+        #background = torch.randn_like(rgb_float) * torch.std(rgb_float) + torch.mean(rgb_float)
+        #background = (background * 255).to(torch.uint8)
+        background = torch.zeros_like(rgb)
 
         return torch.where(alpha > 0, rgb, background)
 
